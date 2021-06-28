61ef321bdfb3744750508196bef7d1f7cfa94a7a
EhWanu <77857074+EhWanu@users.noreply.github.com>
2021-06-28 16:09:39 -0400

    created counter with hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Counter from './Counter';
+import CounterHooks from './CounterHooks';
 
 function App() {
   
   return (
+    <>
     <Counter initialCount={0} />
+    <CounterHooks initialCount={0} />
+    </>
    )
 }
 
