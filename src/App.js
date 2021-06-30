91f023457d3009d98661d9c790f89f186efe5d50
EhWanu <77857074+EhWanu@users.noreply.github.com>
2021-06-30 10:55:43 -0400

    Run Build

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Counter from './Counter';
 import CounterHooks from './CounterHooks';
 
+export const ThemeContext = React.createContext()
+
 function App() {
-  
+  console.log("Render App")
+  const [theme, setTheme] = useState('red')
   return (
-    <>
-    <Counter initialCount={0} />
-    <CounterHooks initialCount={0} />
-    </>
-   )
+    <ThemeContext.Provider value={{ backgroundColor: theme }}>
+      Counter
+      <Counter initialCount={0} />
+      Counter Hooks
+      <CounterHooks initialCount={0} />
+      <button onClick={() => setTheme(prevTheme => {
+        return prevTheme === 'red' ? 'blue' : 'red'
+      })}>Toggle Theme</button>
+    </ThemeContext.Provider>
+  )
 }
 
 export default App;
--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,5 @@
 import React, { Component }  from 'react'
-
+import { ThemeContext } from './App'
 
 export default class Counter extends Component {
     constructor(props) {
@@ -11,15 +11,19 @@ export default class Counter extends Component {
     }
 
     render() {
-        
+        console.log("Render Counter")
         return (
-         <div>
-            <button onClick={() => this.changeCount(-1)}>-</button>
-            <span>{this.state.count}</span>
-            <button onClick={() => this.changeCount(1)}>+</button>
-         </div>
+          <ThemeContext.Consumer>
+            {style => (
+              <div>
+                <button style={style} onClick={() => this.changeCount(-1)}>-</button>
+                <span>{this.state.count}</span>
+                <button style={style} onClick={() => this.changeCount(1)}>+</button>
+              </div>
+            )}
+          </ThemeContext.Consumer>
         )
-    }
+      }
     
         changeCount(amount) {
             this.setState(prevState => { 
